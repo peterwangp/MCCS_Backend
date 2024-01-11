1dbf4d0006f21b01da47549fcf10cb5860d50dca
peterwangp <peterwangp@gmail.com>
2024-01-11 15:56:54 -0500

    Update UserRepository.java

--- src/main/java/com/example/registration/repository/UserRepository.java
+++ src/main/java/com/example/registration/repository/UserRepository.java
@@ -4,9 +4,6 @@ import com.example.registration.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * Created by rajeevkumarsingh on 27/06/17.
- */
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
